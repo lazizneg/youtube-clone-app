679968cd2d31e98893f08f341b3bfecebb1c7886
Lazizjon <ln41128n@pace.edu>
2023-02-04 17:09:00 -0500

    Working on Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,7 +3,14 @@ import { Box, Stack, Typography } from "@mui/material";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 }}}>
+        Sidebar
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright @ Lazizjon Negmatullaev
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Navbar } from './Navbar';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as SearchBar } from './SearchBar';
-
+export { default as Sidebar } from './Sidebar'
