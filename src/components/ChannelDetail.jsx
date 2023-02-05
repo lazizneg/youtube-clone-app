0307772808ceacb37a80c6a2dc60b8ef0cc1e115
Lazizjon <ln41128n@pace.edu>
2023-02-04 20:37:27 -0500

    Updated ChannelCard.jsx and ChannelDetail.jsx

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+    .then((data) => setVideos(data?.items));
+  }, [id])
+
   return (
     <div>ChannelDetail</div>
   )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
 
 const options = {
     url: BASE_URL,
     params: {
-        part: 'snippet', videoId: 'M7FIvfx5J10'},
+      relatedToVideoId: '7ghhRHRP6t4',
+      part: 'id,snippet',
+      type: 'video',
+      maxResults: '50'
+    },
     headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
