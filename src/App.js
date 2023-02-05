e498f31f5cc140d3dc28eed6f2cb33eda81e462b
Lazizjon <ln41128n@pace.edu>
2023-02-05 14:49:34 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { Box } from '@mui/material';
 
 import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
-const App = () =>  (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: '#000' }}>
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Feed />} />
-          <Route path='/video/:id' element={<VideoDetail />} />
-          <Route path='/channel/:id' element={<ChannelDetail />} />
-          <Route path='/search/:searchTerm' element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
-  );
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App
+export default App;
