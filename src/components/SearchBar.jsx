0feb9148f65a534b6e2e0dfc40eb57530fe15919
Lazizjon <ln41128n@pace.edu>
2023-02-05 15:06:27 -0500

    Updated SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -7,7 +7,7 @@ const SearchBar = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
+    const onhandleSubmit = (e) => {
         e.preventDefault();
 
         if (searchTerm) {
@@ -19,26 +19,26 @@ const SearchBar = () => {
     return (
         <Paper
             component='form'
-            onSubmit={handleSubmit}
-            sx={{
-                borderRadius: 20,
-                border: '1px solid #e3e3e3',
-                pl: 2,
-                boxShadow: 'none',
-                mr: { sm: 5 },
-            }}
+            onSubmit={onhandleSubmit}
+        sx={{
+            borderRadius: 20,
+            border: '1px solid #e3e3e3',
+            pl: 2,
+            boxShadow: 'none',
+            mr: { sm: 5 },
+        }}
         >
-        <input 
-            className='search-bar'
-            placeholder='Search...'
-            value=""
-            onChange={(e) => setSearchTerm(e.target.value)}
-        />
-        <IconButton type='submit' sx={{ p: '10px', color: 'red' }} >
-            <SearchIcon />
-        </IconButton>
+            <input
+                className='search-bar'
+                placeholder='Search...'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+                <SearchIcon />
+            </IconButton>
         </Paper>
     );
 };
 
-export default SearchBar
+export default SearchBar;
