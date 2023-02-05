cc81a141b497d3b2fd8984640e166b2f7752fdb3
Lazizjon <ln41128n@pace.edu>
2023-02-04 19:33:52 -0500

    Updated Feed.jsx & Sidebar.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = () => {
+const Videos = ({ videos }) => {
   return (
     <div>Videos</div>
   )
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/captions';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com/';
 
 const options = {
     url: BASE_URL,
@@ -11,3 +11,8 @@ const options = {
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
     },
   };
+
+export const fetchFromAPI = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
