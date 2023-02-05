ad3e49e2bec1e623fbda2c4688a4d5ab125c5b80
Lazizjon <ln41128n@pace.edu>
2023-02-05 14:51:50 -0500

    Updated fetchFromAPI.js

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,21 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com/search';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
-    relatedToVideoId: '7ghhRHRP6t4',
-    part: 'id,snippet',
-    type: 'video',
-    maxResults: '50'
+    maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': '674b1ac75bmsh73f85627041ac7cp11b677jsn1e1cce714c32',
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
 
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
 };
